a0552c092b6deb348d24d004e7ba4ee36a8d3047
Steve Phillips <steve@tryingtobeawesome.com>
2018-12-21 01:15:33 -0800

    When assigning tasks, show other projects last (after user(name)s), not first

--- src/utils/suggestions.js
+++ src/utils/suggestions.js
@@ -1,3 +1,5 @@
+import { PURSUANCE_DISPLAY_PREFIX } from '../constants';
+
 export const filterSuggestion = (value, suggestionsObj) => {
   const suggestionsArr = Object.values(suggestionsObj);
   const lowerCaseVal = value.toLowerCase();
@@ -10,5 +12,11 @@ export const filterSuggestion = (value, suggestionsObj) => {
 };
 
 const sortBySuggest = (suggest1, suggest2) => {
+  if ((suggest1.suggestionName.startsWith(PURSUANCE_DISPLAY_PREFIX) &&
+       !suggest2.suggestionName.startsWith(PURSUANCE_DISPLAY_PREFIX)) ||
+      (suggest2.suggestionName.startsWith(PURSUANCE_DISPLAY_PREFIX) &&
+       !suggest1.suggestionName.startsWith(PURSUANCE_DISPLAY_PREFIX))) {
+     return suggest2.suggestionName.localeCompare(suggest1.suggestionName);
+  }
   return suggest1.suggestionName.localeCompare(suggest2.suggestionName);
 };
